24fac901662b0bd8a5aaf1041e8d51e32ba9934d
Rod Sorensen <rodsorensen@hotmail.com>
2014-03-24 17:46:44 -0700

    car club2 app setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarClub2::Application.config.secret_key_base = 'ee6beaa9231b43f2b3fa6ec2b6c9e3e33c6833a254c8ee0ef1a025057e71b988dd3da16e6f9347ec724025ae1586a89653c84692b9581836b6713ea650fb424f'
